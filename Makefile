1b8b1b825b8c60e1415c455a0377c69717ba078d
Nicholas Crane <nickcrusher30@gmail.com>
2023-01-25 21:55:32 -0500

    Initial commit

---
+++ Makefile
@@ -0,0 +1,40 @@
+# To build:
+# 	$ make
+#
+# To compile and run:
+# 	$ make run
+
+# define compiler and output name
+CXX    := gcc
+OUTPUT := game
+
+# manually specify SFML directory
+SFML_DIR := .
+
+# compiler and linker flags
+CXX_FLAGS := -O3 -std=c89
+INCLUDES  := -I./inc
+LDFLAGS   := -O3 -lglfw -lGL -lX11 -lpthread -lXrandr -lXi -ldl
+
+# the source files for the ecs game engine
+SRC_FILES := $(wildcard src/*.c src/**/*.c)
+OBJ_FILES := $(SRC_FILES:.c=.o)
+
+# all of these targets will be made if you just type make
+all:$(OUTPUT)
+
+# define the main executable requirements / command
+$(OUTPUT): $(OBJ_FILES) Makefile
+	$(CXX) $(OBJ_FILES) $(LDFLAGS) -o ./bin/$@
+
+# specifies how the object files are compiled from cpp files
+.c.o:
+	$(CXX) -c $(CXX_FLAGS) $(INCLUDES) $< -o $@
+
+# typing 'make clean' will remove all intermediate build files
+clean:
+	rm -f $(OBJ_FILES) ./bin/$(OUTPUT)
+
+# typing 'make run' will compile and run the program
+run: $(OUTPUT)
+	cd bin && ./$(OUTPUT) && cd ..
---
+++ src/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#define WINDOW_WIDTH 1600
+#define WINDOW_HEIGHT 900
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    GLFWwindow *window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "2D GS", NULL, NULL);
+    if (window == NULL) {
+        printf("Failed to create GLFW window\n");
+        glfwTerminate();
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        printf("Failed to initialize GLAD\n");
+        glfwTerminate();
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
